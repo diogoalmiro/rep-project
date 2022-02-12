7d9d2be4a591dadd371d7e85d28b62b11c96a720
Diogo Almiro <13120173+diogoalmiro@users.noreply.github.com>
2022-02-12 00:40:19 +0000

    Restructure main & parsing flags w/ click

--- launch.py
+++ launch.py
@@ -1,17 +1,17 @@
-from re import search
-import subprocess
-import sys
+from email.policy import default
 import os
 import sqlite3
+import subprocess
+import sys
 import threading
-from flask import Flask, request, session, g, redirect, url_for, abort, \
-     render_template, flash, jsonify, Response, send_file
-import pandas as pd
-from io import BytesIO
 from datetime import date, datetime
+from io import BytesIO
+import click
+
+import pandas as pd
+from flask import Flask, jsonify, render_template, request, send_file
 
 import rep_database
-import rep_dataset
 
 DATABASE_NAME = "rep-database.db"
 
@@ -27,28 +27,15 @@ if "DGERT_LOGIN_PASSWORD" not in os.environ:
 
 dgert_password = os.environ["DGERT_LOGIN_PASSWORD"]
 
-if not os.path.isfile(DATABASE_NAME):
-    # Create the database
-    print("Creating database for the first time. This operation may take a while...")
-    try:
-        rep_database.repDatabase()
-        print("Database created successfully.")
-    except Exception as e:
-        print("Error creating database:\n ", e)
-        os.remove(DATABASE_NAME)
-        sys.exit(1)
-
 if "SQLITE_WEB_PASSWORD" not in os.environ:
-    print("Hint: set the password with the environment variable: SQLITE_WEB_PASSWORD", file=sys.stderr)
+    print("Hint: set the password for sqlite_web with the environment variable: SQLITE_WEB_PASSWORD", file=sys.stderr)
 
 # Start sqlite_web server on a new thread at port 8090
-def run_sqlite_web():
-    subprocess.run(["sqlite_web",DATABASE_NAME,"--port","8090","--no-browser", "--host","0.0.0.0", "--password", "--read-only"])
+def run_sqlite_web(host="0.0.0.0", port=8090):
+    subprocess.run(["sqlite_web",DATABASE_NAME,"--port",port,"--no-browser", "--host",host, "--password", "--read-only"])
 
-threading.Thread(target=run_sqlite_web).start()
 
 updating = False
-
 def regular_update():
     global updating
     print("Updating database...")
@@ -63,12 +50,8 @@ def regular_update():
         # Sleep for 15min
         threading.Timer(900, regular_update).start()
 
-threading.Timer(86400, regular_update).start()
-
 # Create a new flask web server
 app = Flask(__name__)
-app.config['TEMPLATES_AUTO_RELOAD'] = True
-#app.config['JSON_AS_ASCII'] = False
 
 @app.route("/")
 def index():
@@ -150,7 +133,7 @@ def export():
     strIO = BytesIO()
     excel_writer = pd.ExcelWriter(strIO, engine="xlsxwriter")
 
-    IDS = [];
+    IDS = []
 
     col_names = ["Código Identificador da Organização" , "Tipo de Organização" , "Nome da Organização", "Acrónimo", "Concelho da Sede", "Distrito da Sede", "Setor", "Data da Primeira Atividade Registada", "Data da Última Atividade Registada", "Ativa ou Extinta"]
     rows = connection.execute("""
@@ -296,4 +279,29 @@ def org_patronal_by_year():
         results.append(curr_obj)
     return jsonify(results)
 
-app.run(host="0.0.0.0", port=8080)
+@click.command()
+@click.option('--host', help='webapp server host.', show_default=True, default="127.0.0.1")
+@click.option('--port', help='webapp server port.', show_default=True, default="8080")
+@click.option('--sqlite-host', help='sqlite_web server host.', show_default=True, default="127.0.0.1")
+@click.option('--sqlite-port', help='sqlite_web server port.', show_default=True, default="8090")
+def main(host="127.0.0.1", port="8080", sqlite_host="127.0.0.1", sqlite_port="8090"):
+    if not os.path.isfile(DATABASE_NAME):
+        # Create the database
+        print("Creating database for the first time. This operation may take a while...")
+        try:
+            rep_database.repDatabase()
+            print("Database created successfully.")
+        except Exception as e:
+            print("Error creating database:\n ", e)
+            os.remove(DATABASE_NAME)
+            sys.exit(1)
+    
+    threading.Thread(target=run_sqlite_web, args=(sqlite_host, sqlite_port)).start()
+    # update database in background every day
+    threading.Timer(86400, regular_update).start()
+    app.run(host=host, port=port)
+
+
+if __name__ == "__main__":
+    main()
+
